d9ef7f005a372caf1ab46866bd6021721aaff869
Domonkos Skublics <skubidomi@gmail.com>
2020-09-22 07:23:04 +0200

    fix and commit this 2 functions from the auto backup

--- sub_02/BIP2020_Lab02_Code_package/find_the_duck.m
+++ sub_02/BIP2020_Lab02_Code_package/find_the_duck.m
@@ -1,3 +1,7 @@
 function duck = find_the_duck(I)
-	
+    R = squeeze(I(:,:,1));
+    G = squeeze(I(:,:,2));
+    B = squeeze(I(:,:,3));
+
+    duck = B<120;
 end
--- sub_02/BIP2020_Lab02_Code_package/find_the_pine.m
+++ sub_02/BIP2020_Lab02_Code_package/find_the_pine.m
@@ -1,3 +1,13 @@
 function pine = find_the_pine(I)
+    R = squeeze(I(:,:,1));
+    G = squeeze(I(:,:,2));
+    B = squeeze(I(:,:,3));    
     
+    I_hsv = rgb2hsv(I);
+    
+    H = squeeze(I_hsv(:,:,1));
+    S = squeeze(I_hsv(:,:,2));
+    V = squeeze(I_hsv(:,:,3));
+    
+    pine = H<0.13 | H>0.7;
 end
