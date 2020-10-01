f62ae970a6fbe89b66f95150b848b8298c5543bd
Domonkos Skublics <skubidomi@gmail.com>
2020-10-01 11:10:00 +0200

    week3 exc4

--- sub_03/BIP2020_Lab03__Code_package/calc_hist_vector.m
+++ sub_03/BIP2020_Lab03__Code_package/calc_hist_vector.m
@@ -1,3 +1,9 @@
 function hist_vector = calc_hist_vector(input_img)
-    
+    hist_vector = zeros(1,256);
+    for r = 1:size(input_img, 1) %rows
+        for c = 1:size(input_img, 2) %columns
+            value = input_img(r,c);
+            hist_vector(value+1) = hist_vector(value+1)+1;
+        end
+    end
 end
