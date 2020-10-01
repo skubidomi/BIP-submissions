65cf4e6490b77493618414f10c8fc8540203bb7b
Domonkos Skublics <skubidomi@gmail.com>
2020-10-01 11:24:13 +0200

    week3 exc5

--- sub_03/BIP2020_Lab03__Code_package/stretch_lin.m
+++ sub_03/BIP2020_Lab03__Code_package/stretch_lin.m
@@ -1,3 +1,8 @@
 function output_img = stretch_lin(input_img)
+    img_max = double(max(input_img(:)));%vectorize with (:)
+    img_min = double(min(input_img(:)));
     
+    img_double = double(input_img);
+    output_img = (255/(img_max-img_min)) * (img_double-img_min);
+    output_img = uint8(output_img);
 end
