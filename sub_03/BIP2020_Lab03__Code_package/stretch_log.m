789795c3f60dd6d07bce0dab364ce765c6778b1e
Domonkos Skublics <skubidomi@gmail.com>
2020-10-01 12:50:41 +0200

    week3 exc6

--- sub_03/BIP2020_Lab03__Code_package/stretch_log.m
+++ sub_03/BIP2020_Lab03__Code_package/stretch_log.m
@@ -1,3 +1,10 @@
 function output_img = stretch_log(input_img, c)
+    img_double = double(input_img); % 
+    log_img = c*log(img_double+1);
     
+    img_max = double(max(log_img(:)));%vectorize with (:)
+    img_min = double(min(log_img(:)));
+    
+    output_img = (255/(img_max-img_min)) * (log_img-img_min);
+    output_img = uint8(output_img);
 end
