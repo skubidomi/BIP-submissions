177201fb5b1f4a9776a86ff5c995b46ccdfcbab7
Domonkos Skublics <skubidomi@gmail.com>
2020-10-05 17:34:00 +0200

    week4 exc1

--- sub_04/my_fourier.m
+++ sub_04/my_fourier.m
@@ -1,3 +1,12 @@
 function [F] = my_fourier(I)
-    
+    F = complex(zeros(size(I, 1), size(I, 2)));
+    for k1 = 0:size(F, 1)-1
+        for k2 = 0:size(F, 2)-1
+            for n1 = 0:size(I, 1)-1
+                for n2 = 0:size(I, 2)-1
+                    F(k1+1, k2+1) = F(k1+1, k2+1) + I(n1+1, n2+1)*exp(-1j*(2*pi/size(I, 1))*k1*n1)*exp(-1j*(2*pi/size(I, 2))*k2*n2);
+                end
+            end
+        end
+    end
 end
