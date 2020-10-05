80d2d6cd2bfbc205a21c822e501b376c27922da8
Domonkos Skublics <skubidomi@gmail.com>
2020-10-05 19:48:50 +0200

    week4 exc3

--- sub_04/mask_fourier.m
+++ sub_04/mask_fourier.m
@@ -1,3 +1,14 @@
 function newF = mask_fourier(F, x, y, r)
+    F = fftshift(F);
+    x = round(x); % x, y are vectors
+    y = round(y);
     
+    for i = 1:size(x,2)
+        x_1 = max([1 x(i)-r]);
+        x_2 = min([x(i)+r size(F,1)]);
+        y_1 = max([1 y(i)-r]);
+        y_2 = min([y(i)+r size(F,2)]);
+        F(y_1:y_2, x_1:x_2) = 0+0i;
+    end
+    newF = ifftshift(F);
 end
