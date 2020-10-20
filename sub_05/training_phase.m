4576a99bbfd2c8b744c23507998f9ae27d2e37d5
Domonkos Skublics <skubidomi@gmail.com>
2020-10-20 11:01:47 +0200

    lab 5 exc2

--- sub_05/training_phase.m
+++ sub_05/training_phase.m
@@ -1,3 +1,12 @@
 function MODEL = training_phase(T_cell)
-    
+    N = size(T_cell,2);
+    MODEL = zeros(N,9);
+    for n=1:N % texture index
+        for k=1:9 % filter kernel index
+            % convolve training texture with kernel
+            A = conv2(T_cell{n},laws_kernel(k),'same');
+            
+            MODEL(n,k) = 1/(size(T_cell{n},1)*size(T_cell{n},2)) * sum(sum(A.^2));
+        end
+    end
 end
