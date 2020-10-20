8ebc696d89ff72b45c9fee87068364178097c196
Domonkos Skublics <skubidomi@gmail.com>
2020-10-20 14:43:13 +0200

    lab 5 exc4

--- sub_05/majority_voting.m
+++ sub_05/majority_voting.m
@@ -1,3 +1,14 @@
 function OUT = majority_voting(IN, w_dia)
-    
+    OUT = zeros(size(IN));
+    for x=1:w_dia:size(IN,1)
+        for y=1:w_dia:size(IN,2)
+            x_start = x;
+            x_end = min(x+w_dia-1,size(IN,1));
+            y_start = y;
+            y_end = min(y+w_dia-1,size(IN,2));
+            SelectedWindow = IN(x_start:x_end,y_start:y_end);
+            MajorityValue = mode(mode(SelectedWindow));
+            OUT(x_start:x_end,y_start:y_end) = MajorityValue;
+        end
+    end
 end
