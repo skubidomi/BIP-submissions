a78ede1c1bd5bc053062f4b8fbf9a7d9f951f980
Domonkos Skublics <skubidomi@gmail.com>
2020-10-26 11:45:34 +0100

    lab6 exc4

--- sub_06/restoration_wiener_white.m
+++ sub_06/restoration_wiener_white.m
@@ -1,3 +1,9 @@
 function [x_tilde] = restoration_wiener_white(y, h, var_n)
+    Y = fft2(y);
+    H = psf2otf(h,size(Y));
+    NSPR = var_n/var(y(:));
     
+    % we assume white noise, so:
+    R = conj(H) ./ (abs(H).^2 + NSPR);
+    x_tilde = abs(ifft2(R.*Y));
 end
