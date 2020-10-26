d7f505b28f021eef763d91e238bec539ec6ed77c
Domonkos Skublics <skubidomi@gmail.com>
2020-10-26 11:24:59 +0100

    lab6 exc3

--- sub_06/restoration_wiener.m
+++ sub_06/restoration_wiener.m
@@ -1,3 +1,11 @@
 function [x_tilde] = restoration_wiener(y, h, n)
+    Y = fft2(y);
+    N = fft2(n);
+    H = psf2otf(h,size(Y));
     
+    P_YY = Y .* conj(Y);
+    P_NN = N .* conj(N);
+    
+    R = conj(H)./(abs(H).^2 + P_NN./P_YY);
+    x_tilde = abs(ifft2(R.*Y));
 end
