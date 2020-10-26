ec9a70bb5474e5de277c46ca47e1d5724d4d7f62
Domonkos Skublics <skubidomi@gmail.com>
2020-10-26 14:05:05 +0100

    lab6 exc5

--- sub_06/blur_edge.m
+++ sub_06/blur_edge.m
@@ -1,3 +1,14 @@
 function out_img = blur_edge(in_img)
+    hsize = 60;
+    sigma = 10;
+    psf = fspecial('gaussian',hsize,sigma); % point spread function
+    otf = psf2otf(psf,size(in_img)); % optical transfer function
     
+    blurred_img = abs(ifft2(otf.*fft2(in_img)));
+    % replace the edges with the blurred image
+    wm = ones(70);
+    wm = padarray(wm,[1 1],0,'both');
+    wm2 = imresize(wm,size(in_img),'bilinear');
+    wm2 = mat2gray(wm2,[0, 1]);
+    out_img = in_img.*wm2 + blurred_img.*(1-wm2);
 end
