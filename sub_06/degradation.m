470a5c71961ade270319fb4afd6bb6088a1cc0bc
Domonkos Skublics <skubidomi@gmail.com>
2020-10-26 09:33:22 +0100

    lab6 exc1

--- sub_06/degradation.m
+++ sub_06/degradation.m
@@ -1,3 +1,7 @@
 function [y, h, n] = degradation(x)
-    
+    len = 42;
+    theta = 30;
+    h = fspecial('motion', len, theta);
+    n = sqrt(0.001) * randn(size(x));
+    y = imfilter(x,h,'replicate','same','conv') + n; 
 end
