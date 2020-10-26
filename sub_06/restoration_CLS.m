84a9410abf6bafc58aae418a3ae21502954bc12a
Domonkos Skublics <skubidomi@gmail.com>
2020-10-26 11:05:17 +0100

    modify spacing (no functionality change)

--- sub_06/restoration_CLS.m
+++ sub_06/restoration_CLS.m
@@ -2,5 +2,5 @@ function [x_tilde] = restoration_CLS(y, h, alpha, c)
     Y = fft2(y);
     H = psf2otf(h,size(Y));
     R = conj(H)./(abs(H).^2 + alpha*abs(psf2otf(c,size(Y))).^2);
-    x_tilde = abs(ifft2(R .* Y));
+    x_tilde = abs(ifft2(R.*Y));
 end
