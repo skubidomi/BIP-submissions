51a7961f016867b1c169761ca88566e1a5b2cda7
Domonkos Skublics <skubidomi@gmail.com>
2020-11-03 12:08:18 +0100

    lab7 exc2

--- sub_07/step2_assignment.m
+++ sub_07/step2_assignment.m
@@ -1,3 +1,10 @@
 function LUT = step2_assignment(S, k, LUT, M)
-    
+    m = size(S,1);
+    d_ij = zeros(m,k);
+    for i = 1:m
+        for j = 1:k
+            d_ij(i,j) = sum((S(i,:)-M(j,:)).^2);
+        end
+        [minimum,LUT(i)] = min(d_ij(i,:));
+    end
 end
