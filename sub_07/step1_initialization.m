85eff7c506815efff59e9916c6ac8d6c1efd8103
Domonkos Skublics <skubidomi@gmail.com>
2020-11-03 10:34:15 +0100

    lab7 exc1

--- sub_07/step1_initialization.m
+++ sub_07/step1_initialization.m
@@ -1,3 +1,11 @@
 function [LUT, M] = step1_initialization(S, k)
+    m = size(S,1); % number of elements in S space
+    LUT = zeros(1,m);
     
+    n = size(S,2);
+    M = zeros(k,n); % k is clusters, n is dimension -> S second axis
+    
+    % choose every x-th element to able to reproduce the result
+    diff = floor(m/k);
+    M = S(1:diff:diff*k,:);
 end
