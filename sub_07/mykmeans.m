0d82087c2ecadf2eded44be9ba402eb95ea16bb1
Domonkos Skublics <skubidomi@gmail.com>
2020-11-04 15:23:07 +0100

    lab7 exc4

--- sub_07/mykmeans.m
+++ sub_07/mykmeans.m
@@ -1,5 +1,15 @@
 function [LUT, M] = mykmeans(S, k)
-    
+    [LUT, M] = step1_initialization(S, k); % initialization
+    eps = 0.02; 
+    converged = false; % for checking the distance between the old and new cluster centers
+    t = 0; % iteration counter
+    while (~converged) && (t<100)
+        LUT = step2_assignment(S, k, LUT, M); % assignment step
+        M_new = step3_update(S, k, LUT, M); % update and save into an variable to make a comparison
+        converged = sum(sum((M_new-M).^2)) < eps;
+        M = M_new;
+        t = t + 1;
+    end
 
     fprintf('Iteration finished after %d steps.\n', t);
 
