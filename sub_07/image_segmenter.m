ba041365b089f619c26afe84c05a74d8ef4b7bdc
Domonkos Skublics <skubidomi@gmail.com>
2020-11-04 16:27:06 +0100

    lab7 exc5

--- sub_07/image_segmenter.m
+++ sub_07/image_segmenter.m
@@ -1,3 +1,9 @@
 function [O] = image_segmenter(I, k)
-    
+    S = reshape(I,size(I,1)*size(I,2),size(I,3)); % convert to the proper format
+    [LUT, M] = mykmeans(S,k);
+    A = zeros(size(LUT,2),size(M,2)); % A will be the segmented image after kmeans algorithm
+    for i = 1:size(LUT,2)
+        A(i,:) = M(LUT(i),:);
+    end
+    O = reshape(A,size(I,1),size(I,2),size(I,3)); % convert it back
 end
