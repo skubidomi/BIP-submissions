b99fb26765dbb93ee96ac1a148d93dd0116bf853
Domonkos Skublics <skubidomi@gmail.com>
2020-11-04 12:21:47 +0100

    lab7 exc3

--- sub_07/step3_update.m
+++ sub_07/step3_update.m
@@ -1,3 +1,7 @@
 function M = step3_update(S, k, LUT, M)
-    
+    for j = 1:k % iterate through M
+        LUT_sel = (LUT==j); 
+        S_sel = S(LUT_sel,:); % select S elements where the LUT_selected is 1
+        M(j,:) = mean(S_sel); % update M with the new cluster center      
+    end
 end
