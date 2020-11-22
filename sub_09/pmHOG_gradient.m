fe3ba1d2d3cf7e87800c076cdf352b6e29d291c5
Domonkos Skublics <skubidomi@gmail.com>
2020-11-22 11:55:19 +0100

    lab9 exc1

--- sub_09/pmHOG_gradient.m
+++ sub_09/pmHOG_gradient.m
@@ -1,3 +1,13 @@
 function [PHI, MAG] = pmHOG_gradient(I)   
+    % 1
     I = double(I);
+    % 2
+    H_x = [-1 0 1];
+    H_y = [-1; 0; 1];
+    % 3
+    X = imfilter(I,H_x,'replicate','same');
+    Y = imfilter(I,H_y,'replicate','same');
+    PHI = rad2deg(atan(Y./X));
+    MAG = sqrt(X.^2+Y.^2);
+    
 end
