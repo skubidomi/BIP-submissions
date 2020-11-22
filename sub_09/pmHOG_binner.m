cdc645a36e6137890fa03643d8ee1fd97c984378
Domonkos Skublics <skubidomi@gmail.com>
2020-11-22 11:55:40 +0100

    lab9 exc2

--- sub_09/pmHOG_binner.m
+++ sub_09/pmHOG_binner.m
@@ -1,3 +1,11 @@
 function H = pmHOG_binner(PHI, MAG)
-    
+    % 1
+    H = zeros(1,9);
+    % 2
+    for binindex = 1:size(H,2)
+        mini = -90 + (binindex-1)*20; % min and max value of the specific interval
+        maxi = -90 + binindex*20;
+        LOG_MASK = (PHI >= mini & PHI < maxi);% select the proper elements from the phi matrix which belong to this bin
+        H(binindex) = sum(MAG(LOG_MASK)); % store the corresponding magnitude elements in the actual bin of the histogram
+    end
 end
