9f7cefd7c0fb6c0c71792ee232e827c964c72ad1
Domonkos Skublics <skubidomi@gmail.com>
2020-11-23 14:38:48 +0100

    lab9 exc4

--- sub_09/detectPedestrians.m
+++ sub_09/detectPedestrians.m
@@ -1,3 +1,8 @@
 function [score] = detectPedestrians(I, sample_HOG)
-    
+    % 1
+    HOG = pmHOG_extractHOG(I);
+    HOG = HOG.*sample_HOG;
+    LOG_MASK = HOG < 0.01;
+    HOG(LOG_MASK) = 0;
+    score = nansum(HOG(:));
 end
