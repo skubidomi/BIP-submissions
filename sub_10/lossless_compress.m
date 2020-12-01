070296bf977794fa6aa4de88b77833a6b0c9b8af
Domonkos Skublics <skubidomi@gmail.com>
2020-12-01 20:12:09 +0100

    lab10 exc1

--- sub_10/lossless_compress.m
+++ sub_10/lossless_compress.m
@@ -1,3 +1,21 @@
 function [compressed_image] = lossless_compress(noncompressed_image)
-    
+    H = size(noncompressed_image,1);
+    W = size(noncompressed_image,2);
+    compressed_image = cell(H,1);
+    for y = 1:H
+        symbol = noncompressed_image(y,1);
+        counter = 1;
+        row_desc = symbol;
+        for x = 2:W
+            if symbol == noncompressed_image(y,x)
+                counter = counter + 1;
+            else
+                symbol = 1 - symbol;
+                row_desc = [row_desc, counter];
+                counter = 1;
+            end
+        end
+        row_desc = [row_desc, counter];
+        compressed_image{y} = row_desc;
+    end
 end
