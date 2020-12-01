4ea221d00cbfff4da717d398be8e4887d8d2b40c
Domonkos Skublics <skubidomi@gmail.com>
2020-12-01 20:37:59 +0100

    lab10 exc1b

--- sub_10/lossless_decompress.m
+++ sub_10/lossless_decompress.m
@@ -1,3 +1,15 @@
 function [decompressed_image] = lossless_decompress(compressed_image)
-    
+    decompressed_image = [];
+    H = size(compressed_image,1);
+    for y = 1:H
+        symbol = compressed_image{y}(1);
+        row_pixels = [];
+        N = size(compressed_image{y},2);
+        for x = 2:N
+            block = repmat(symbol,1,compressed_image{y}(x));
+            row_pixels = [row_pixels, block];
+            symbol = 1 - symbol;
+        end
+        decompressed_image = [decompressed_image; row_pixels];
+    end
 end
