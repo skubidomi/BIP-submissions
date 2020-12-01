5a7c68901f040cd94a5ee5c5cf0a98e2a42a86e4
Domonkos Skublics <skubidomi@gmail.com>
2020-12-01 21:43:19 +0100

    lab10 exc2

--- sub_10/lossy_compress.m
+++ sub_10/lossy_compress.m
@@ -1,3 +1,15 @@
 function [compressed] = lossy_compress(input_image, block_size, k)
-    
+    cut_size = floor(size(input_image)/block_size) * block_size;
+    input_image = double(input_image);
+    LIST = [];
+    for r = 1:block_size:cut_size(1)
+        for c = 1:block_size:cut_size(2)
+            crop = input_image(r:r+block_size-1, c:c+block_size-1);
+            LIST = [LIST; crop(:)'];
+        end
+    end
+    [LUT,DICT] = kmeans(LIST,k);
+    compressed.DICT = DICT;
+    compressed.LUT = LUT;
+    compressed.cut_size = cut_size;
 end
--- sub_10/lossy_decompress.m
+++ sub_10/lossy_decompress.m
@@ -1,3 +1,16 @@
 function [decompressed] = lossy_decompress(compressed)
-    
+    DICT = compressed.DICT;
+    LUT = compressed.LUT;
+    cut_size = compressed.cut_size;
+    LIST = DICT(LUT,:);
+    decompressed = uint8(zeros(cut_size));
+    block_size = sqrt(numel(LIST(1,:)));
+    k = 1;
+    for r = 1:block_size:cut_size(1)
+        for c = 1:block_size:cut_size(2)
+            part = reshape(LIST(k,:), block_size, block_size);
+            decompressed(r:r+block_size-1, c:c+block_size-1) = part;
+            k = k+1;
+        end
+    end
 end
